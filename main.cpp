713f87ca4ee801a2034dc69c15436bc8e0c7f78d
SozinovAP <alexsozinov73@gmail.com>
2020-04-15 18:21:35 +0300

    edit main

--- main.cpp
+++ main.cpp
@@ -4,26 +4,11 @@
 
 #include<iostream>
 
-#include "Tables/HashTableList.h"
 using namespace std;
 
 
 int main()
 {
-	Polynomial pol("xyz");
-	HashTableList tn(10);
-	tn.Insert("1", pol);
-	tn.Insert("2", pol);
-	tn.Insert("3", pol);
-	HashTableList::iterator it = nullptr;
-	cout << tn.begin()->GetName() << endl;
-	//cout << tn.end()->GetName();
-	/*for (it = tn.begin(); it != tn.end(); ++it)
-	{
-		cout << it->GetName() << endl;
-	}*/
-	int i;
-	cin >> i;
 	TableManager tableManager;
 	TableTreeAVL ta;
 	tableManager.AddTable(&ta);
