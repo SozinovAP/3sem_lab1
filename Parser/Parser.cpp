6f22e97a076a5cbaaa40345487134fd7866a2712
SozinovAP <alexsozinov73@gmail.com>
2020-04-16 20:41:15 +0300

    release Parser

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -1,22 +1,82 @@
 #include "Parser.h"
+#include <stack>
 
-
-string Parser::ToPostfix(string)
+string Parser::ToPostfix(list<Phrase> listPhrase)
 {
-	//string tmp = formula;
-	//Formula newFormula(formula);
-	/*list<Phrase> phrases = newFormula.Parse();
-	for (auto it = phrases.begin(); it != phrases.end(); ++it)
-		cout << (*it).str;
-	return tmp;*/
-	string str;
-	return str;
+	stack<Phrase> stackPhrase;
+	string postfix = "";
+
+	if (!BalanceParentheses(listPhrase))
+		throw "uncorrect";
+	bool F = true;
+	for (auto it = listPhrase.begin(); it != listPhrase.end(); ++it)
+	{
+		//cout << (*it).str << endl;
+		if (it->str == "integral")
+		{
+			postfix += "integral ";
+		}
+		else if ((IsOperator(it->str)) || (it->str == "("))
+		{
+			if(F)
+			{
+				F = false;
+				postfix += " ";
+			}
+			if((stackPhrase.empty()) || (it->str == "("))
+			{
+				stackPhrase.push(*it);
+			}
+			else
+			{
+				if (Parser::PriorityOperator(it->str) > Parser::PriorityOperator(stackPhrase.top().str))
+				{
+					stackPhrase.push(*it);
+				}
+				else
+				{
+					while ((!stackPhrase.empty()) && (Parser::PriorityOperator(stackPhrase.top().str)) >= Parser::PriorityOperator(it->str))
+					{
+						postfix += stackPhrase.top().str;
+						stackPhrase.pop();
+						postfix += " ";
+					}
+					stackPhrase.push(*it);
+				}
+			}
+		}
+		else if (it->str == ")")
+		{
+			if (F)
+			{
+				F = false;
+				postfix += " ";
+			}
+			while (stackPhrase.top().str != "(")
+			{
+				postfix += stackPhrase.top().str;
+				stackPhrase.pop();
+				postfix += " ";
+			}
+			stackPhrase.pop();
+		}
+		else
+		{
+			postfix += it->str;
+			F = true;
+		}
+	}
+
+	postfix += " ";
+	while (!stackPhrase.empty())
+	{
+		postfix += stackPhrase.top().str;
+		stackPhrase.pop();
+		postfix += " ";
+	}
+	return postfix;
 }
 
-bool Parser::IsFormula(string formula)
-{
-	return true;
-}
 
 string Parser::GetFormula(string name)
 {
@@ -24,4 +84,87 @@ string Parser::GetFormula(string name)
 	return tmp;
 }
 
-void Parser::Parse(string str, TableManager manager) {}
+bool Parser::BalanceParentheses(list<Phrase> listPhrase)
+{
+	if (listPhrase.empty())
+		throw "list is empty";
+	stack<char> check;
+	auto it = listPhrase.begin();
+	//поиск 1-ой скобки, если она закрывающа€с€, то расстановка не корректна
+	while((it != listPhrase.end()) && (check.empty()))
+	{
+		if ((*it).str == "(")
+		{
+			check.push('(');
+		}
+		else if ((*it).str == ")")
+		{
+			return false;
+		}
+		++it;
+	}
+	//добавление всех скобок в стек
+	while((it != listPhrase.end()))
+	{
+		if ((*it).str == "(")
+		{
+			check.push('(');
+		}
+		else if ((*it).str == ")")
+		{
+			check.push(')');
+		}
+		++it;
+	}
+
+	//если верхн€€ скобка открывающа€с€, то расстановка некорректна
+	if ((!check.empty()) && (check.top() == '('))
+		return false;
+	int count = 0;
+	//проверка баланса скобок
+	while (!check.empty())
+	{
+		char c = check.top();
+		if (c == ')')
+		{
+			count--;
+		}
+		else
+		{
+			count++;
+		}
+		check.pop();
+	}
+	return (count == 0) ? true : false;
+}
+
+void Parser::Parse(string str, TableManager manager)
+{
+	Formula formula(str);
+	list<Phrase> listPhrases = formula.Parse();
+	string s = Parser::ToPostfix(listPhrases);
+	for (auto it = listPhrases.begin(); it != listPhrases.end(); ++it)
+	{
+		cout << (*it).str << endl;
+		//if (true)
+	}
+	cout << s;
+}
+
+int Parser::PriorityOperator(string s)
+{
+	if (s == "(")
+		return 0;
+	else if (s == ")")
+		return 1;
+	else if ((s == "+") || (s == "-"))
+		return 2;
+	else if ((s == "/") || (s == "*"))
+		return 3;
+}
+
+bool Parser::IsOperator(string s)
+{
+	string oper = "+-*/";
+	return (oper.find(s) == -1) ? false : true;
+}
--- Parser/Parser.h
+++ Parser/Parser.h
@@ -6,10 +6,14 @@
 static class Parser
 {
 private:
-	static string ToPostfix(string);
-	static bool IsFormula(string formula);
+	static string ToPostfix(list<Phrase> listPhrase);
 	static string GetFormula(string name);
+	static bool BalanceParentheses(list<Phrase> listPhrase);
+	static int PriorityOperator(string s);
+	static bool IsOperator(string s);
 public:
 	
 	static void Parse(string str, TableManager manager);
 };
+
+ 
